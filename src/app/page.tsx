964e37ebd4fb152fbb83d5c18a2679fc40ec69d0
Mark Carson <markcarson121@gmail.com>
2024-09-23 12:03:33 -0300

    adjusted front-end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,26 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <div className="max-w-screen-lg mx-auto my-8">
-        <h1 className="font-bold text-xl">Balloon animals for sale</h1>
-        <p>Quick and time-saving. Perfect for lazy clowns everywhere.</p>
-        <section className="flex flex-col my-4">
-          <div className="w-fit">
-            <Image
-              src="https://images.unsplash.com/photo-1519658493417-b687d62a3111?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="rounded"
-              alt="Blue Dog"
-              height={300}
-              width={200}
-            />
-            <p className="my-2">Blue dog.</p>
-            <Link className="hover:underline font-extrabold" href="/checkout">
-              👉 Buy Now! 👈
-            </Link>
-          </div>
+    <>
+      <nav className="h-16 bg-blue-500 flex items-center">StripeStore</nav>
+      <main className="container mx-auto py-10 border border-red-500">
+        <h1 className="font-bold text-xl">New listings</h1>
+        <section className="border border-green-500">
+          <Image
+            src="https://images.unsplash.com/photo-1519658493417-b687d62a3111?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Blue Dog"
+            height={300}
+            width={200}
+          />
+          <p className="my-2">Blue dog.</p>
+          <Link
+            className="bg-blue-500 text-white p-4 rounded hover:bg-blue-600"
+            href="/checkout"
+          >
+            Buy Now
+          </Link>
         </section>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
