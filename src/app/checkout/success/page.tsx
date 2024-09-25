835480dbee534b7285640de549fe5d6464805274
Mark Carson <markcarson121@gmail.com>
2024-09-25 10:20:49 -0300

    adjusted sizes for success page

--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -8,31 +8,33 @@ import stripe from "/public/images/stripe.svg";
 export default function Page() {
   return (
     <main className="relative flex h-screen flex-col items-center justify-center bg-pink-100">
-      <section className="flex h-full w-screen flex-col overflow-clip bg-white shadow lg:h-2/3 lg:w-1/2">
+      <section className="flex h-full w-screen flex-col items-center overflow-clip bg-white shadow lg:h-1/2 lg:w-1/3">
         <header className="w-full border-b-2 border-[#15233c] py-2">
           <Image
             className="mx-auto"
             src={stripe}
             alt="Stripe payment logo"
-            width={96}
-            height={96}
+            width={64}
+            height={64}
             priority
           />
         </header>
         <article
-          className="flex flex-1 flex-col items-center justify-center space-y-6"
+          className="flex w-1/2 flex-1 flex-col items-center justify-center space-y-6"
           role="article"
           aria-labelledby="success-heading"
         >
-          <Image src={check} alt="Success icon" width={72} height={72} />
+          <Image src={check} alt="Success icon" width={56} height={56} />
           <div className="space-y-1 text-center">
-            <h1 id="success-heading" className="text-2xl">
+            <h1 id="success-heading" className="text-xl">
               Thank you for ordering
             </h1>
-            <p className="font-extralight">Your payment has been successful</p>
+            <p className="text-sm font-extralight">
+              Your payment has been successful
+            </p>
           </div>
           <Link
-            className="w-fit rounded bg-pink-100 p-4 font-semibold hover:bg-pink-200"
+            className="w-3/4 rounded bg-pink-100 p-2 text-center text-sm font-semibold hover:bg-pink-200"
             href="/"
             aria-label="Continue shopping and return to the checkout page"
           >
